8541e82887c772a3a3b6f2638385c315a04043f8
Jake Reynolds <jreynoldsdev@gmail.com>
2016-07-14 14:31:29 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#Lazy Fuzzer
+
+Right click on any input field, hover over "Lazy Fuzzer", and select the payload you would like to inject.
+
+<a href="http://superuser.com/questions/247651/how-does-one-install-an-extension-for-chrome-browser-from-the-local-file-system">How to load an unpacked extension</a>
---
+++ background.js
@@ -0,0 +1,44 @@
+// The onClicked callback function.
+function onClickHandler(info, tab) {
+    //For CSV injection and such that require " and '
+    var id = info.menuItemId.replace(/'/g, "\\'").replace(/"/g, '\\"');
+    //Insert the value into the currently selected text field
+    chrome.tabs.executeScript({
+        code: 'document.activeElement.value =\'' + id + '\''
+    })
+};
+
+chrome.contextMenus.onClicked.addListener(onClickHandler);
+
+// Set up context menu tree at install time.
+chrome.runtime.onInstalled.addListener(function() {
+    //Top level menus
+    createContext('XSS');
+    createContext('SQLi');
+    createContext('CSVi');
+    //Sub menus
+    createContext('<b>test</b>', 'XSS');
+    createContext('<script>alert()</script>', 'XSS');
+    createContext('\' or 1=1 -- ', 'SQLi');
+    createContext("\",=cmd|\'/c calc\'!\'c3\',\"", 'CSVi');
+});
+
+//Payload will be the title value
+//If it's a child menu the parent id must be passed in
+function createContext(title, parent) {
+    if (parent) {
+        chrome.contextMenus.create({
+            "title": title,
+            "parentId": parent,
+            "id": title,
+            "contexts": ['editable']
+        });
+    } else {
+
+        chrome.contextMenus.create({
+            "title": title,
+            "id": title,
+            "contexts": ['editable']
+        });
+    }
+}
